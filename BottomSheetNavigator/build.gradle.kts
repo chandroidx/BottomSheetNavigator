4e9cc99a08d4bad3a46e33bf834ec05919b8676d
Yechan Park <yc.park@deepfine.com>
2024-05-16 11:02:19 +0900

    JAVA VERSION TO 17

--- BottomSheetNavigator/build.gradle.kts
+++ BottomSheetNavigator/build.gradle.kts
@@ -23,11 +23,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
     buildFeatures.compose = true
@@ -50,7 +50,7 @@ afterEvaluate {
 
                 groupId = "com.github.chandroidx"
                 artifactId = "BottomSheetNavigator"
-                version = "1.0.0"
+                version = "1.0.2"
             }
 
             register("debug", MavenPublication::class) {
@@ -58,7 +58,7 @@ afterEvaluate {
 
                 groupId = "com.github.chandroidx"
                 artifactId = "BottomSheetNavigator"
-                version = "1.0.0"
+                version = "1.0.2"
             }
         }
     }
